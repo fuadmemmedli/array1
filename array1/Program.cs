57a1ccbacf9969694eb84db3a03de4e58971a83a
Fuad Memedli <fuadmemedli@Fuad-MacBook-Air.local>
2022-11-03 13:49:20 +0400

    inital

---
+++ array1/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] myNum = new int[4] { 1, 2, 3, 4 };
+for (int i = 0; i <= myNum.Length; i++)
+{
+    Console.Write(myNum[i] + ", ");
+
+}
+// 3cu calisma
+/*for(int j = 0; j < myNum.Length; j++)
+{
+    myNum[j]++;
+    Console.Write(myNum[j]);
+}
+*/
